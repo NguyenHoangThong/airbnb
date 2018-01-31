1aacbf688eb9afc5ecb8c6fe01c309868d52d50b
NGUYEN HOANG THONG <nhthong161195@gmail.com>
2018-01-31 13:26:23 +0700

    init commit

---
+++ bash.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# compile scss
+echo Compiling scss
+node-sass -r ./template/scss/ -o ./raw/css/
+
+# compile pug
+echo Compiling pug
+pug ./template/views/ -o ./raw/views/
+
+# copy js
+echo Coping js files
+cp -R ./template/js/* ./raw/js/
+
+echo Done.
